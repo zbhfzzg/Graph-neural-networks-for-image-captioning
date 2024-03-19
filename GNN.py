9969275c8121cc8a032697ef7be9bfb544d72e04
Bohan Zhang <zhanghanfzzg@gmail.com>
2024-03-20 00:31:34 +0800

    Learning 1

--- GNN.py
+++ GNN.py
@@ -1,40 +1,147 @@
+import matplotlib.pyplot as plt
 import networkx as nx
-H = nx.DiGraph()
+from torch_geometric.datasets import Planetoid
+from torch_geometric.transforms import NormalizeFeatures
+import torch
+from sklearn.manifold import TSNE
+import numpy as np
 
-#adding nodes
-H.add_nodes_from([
-  (0, {"color": "blue", "size": 250}),
+dataset = Planetoid(root='data/Planetoid', name='Cora', transform=NormalizeFeatures())
+data = dataset[0]  # Get the first graph object.
 
-  (1, {"color": "yellow", "size": 400}),
+#GCN model
+from torch_geometric.nn import GCNConv
+import torch.nn.functional as F
 
-  (2, {"color": "orange", "size": 150}),
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        torch.manual_seed(1234567)
+        self.conv1 = GCNConv(dataset.num_features, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, dataset.num_classes)
 
-  (3, {"color": "red", "size": 600})
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x
+
+""" model = GCN(hidden_channels=16)
+print(model) """
+
+def visualize(h, color):
+    z = TSNE(n_components=2).fit_transform(h.detach().cpu().numpy())
+
+    plt.figure(figsize=(10,10))
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.scatter(z[:, 0], z[:, 1], s=70, c=color, cmap="Set2")
+    plt.show()
+
+""" model.eval()
+
+out = model(data.x, data.edge_index)
+visualize(out, color=data.y)
+ """
 
 
-])
 
-#adding edges
-H.add_edges_from([
-  (0, 1),
+# Train GCN
+model = GCN(hidden_channels=16)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+criterion = torch.nn.CrossEntropyLoss()
 
-  (1, 2),
+def train():
+      model.train()
+      optimizer.zero_grad()
+      out = model(data.x, data.edge_index)
+      loss = criterion(out[data.train_mask], data.y[data.train_mask])
+      loss.backward()
+      optimizer.step()
+      return loss
 
-  (1, 0),
+def test():
+      model.eval()
+      out = model(data.x, data.edge_index)
+      pred = out.argmax(dim=1)
+      test_correct = pred[data.test_mask] == data.y[data.test_mask]
+      test_acc = int(test_correct.sum()) / int(data.test_mask.sum())
+      return test_acc
 
-  (1, 3),
 
-  (2, 3),
+for epoch in range(1, 101):
+    loss = train()
+    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}')
 
-  (3,0)
+test_acc = test()
+print(f'Test Accuracy: {test_acc:.4f}')
+model.eval()
+out = model(data.x, data.edge_index)
+visualize(out, color=data.y)
 
 
-])
 
-node_colors = nx.get_node_attributes(H, "color").values()
-colors = list(node_colors)
-node_sizes = nx.get_node_attributes(H, "size").values()
-sizes = list(node_sizes)
 
-#Plotting Graph
-nx.draw(H, with_labels=True, node_color=colors, node_size=sizes)
+# GAT model
+from torch_geometric.nn import GATConv
+
+class GAT(torch.nn.Module):
+    def __init__(self, hidden_channels, heads):
+        super().__init__()
+        torch.manual_seed(1234567)
+        self.conv1 = GATConv(dataset.num_features, hidden_channels,heads)
+        self.conv2 = GATConv(heads*hidden_channels, dataset.num_classes,heads)
+
+    def forward(self, x, edge_index):
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = self.conv1(x, edge_index)
+        x = F.elu(x)
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = self.conv2(x, edge_index)
+        return x
+
+model = GAT(hidden_channels=8, heads=8)
+print(model)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
+criterion = torch.nn.CrossEntropyLoss()
+
+def train():
+      model.train()
+      optimizer.zero_grad()
+      out = model(data.x, data.edge_index)
+      loss = criterion(out[data.train_mask], data.y[data.train_mask])
+      loss.backward()
+      optimizer.step()
+      return loss
+
+def test(mask):
+      model.eval()
+      out = model(data.x, data.edge_index)
+      pred = out.argmax(dim=1)
+      correct = pred[mask] == data.y[mask]
+      acc = int(correct.sum()) / int(mask.sum())
+      return acc
+
+val_acc_all = []
+test_acc_all = []
+
+for epoch in range(1, 101):
+    loss = train()
+    val_acc = test(data.val_mask)
+    test_acc = test(data.test_mask)
+    val_acc_all.append(val_acc)
+    test_acc_all.append(test_acc)
+    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Val: {val_acc:.4f}, Test: {test_acc:.4f}')
+
+plt.figure(figsize=(12,8))
+plt.plot(np.arange(1, len(val_acc_all) + 1), val_acc_all, label='Validation accuracy', c='blue')
+plt.plot(np.arange(1, len(test_acc_all) + 1), test_acc_all, label='Testing accuracy', c='red')
+plt.xlabel('Epochs')
+plt.ylabel('Accurarcy')
+plt.title('GATConv')
+plt.legend(loc='lower right', fontsize='x-large')
+plt.savefig('gat_loss.png')
+plt.show()
